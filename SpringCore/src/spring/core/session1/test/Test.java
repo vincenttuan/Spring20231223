b82aec6c4996bc067c5a6dea4fae0a888c08a965
MB-teacher <MB-teacher@MB-207-teacher>
2023-12-23 12:19:50 +0800

    java

--- SpringCore/src/spring/core/session1/test/Test.java
+++ SpringCore/src/spring/core/session1/test/Test.java
@@ -1,10 +1,18 @@
 package spring.core.session1.test;
 
+import java.util.Random;
+
+import spring.core.session1.bean.User;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		User user = new User();
+		user.setId(1);
+		user.setName("John");
+		user.setScore(new Random().nextInt(101));
+		
+		System.out.println(user);
 	}
 
 }
