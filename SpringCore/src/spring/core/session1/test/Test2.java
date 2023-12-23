e805a6045c40075c3040a1f27cb3ae8c870eafb5
MB-teacher <MB-teacher@MB-207-teacher>
2023-12-23 13:41:27 +0800

    java

---
+++ SpringCore/src/spring/core/session1/test/Test2.java
@@ -0,0 +1,17 @@
+package spring.core.session1.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.core.session1.bean.User;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// 指定 xml 設定檔
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-config.xml"); 
+		User user = ctx.getBean(User.class);
+		System.out.println(user);
+	}
+
+}
