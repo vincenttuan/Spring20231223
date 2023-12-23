a294705bc7cb1faef589ac323929cee58c87bb97
MB-teacher <MB-teacher@MB-207-teacher>
2023-12-23 15:32:38 +0800

    java

--- SpringCore/src/spring/core/session2/test/Test.java
+++ SpringCore/src/spring/core/session2/test/Test.java
@@ -4,11 +4,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import spring.core.session2.bean.Author;
+import spring.core.session2.bean.Book;
 
 public class Test {
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-config2.xml");
 		Author author = ctx.getBean("author", Author.class);
 		System.out.println(author);
+		Book book = ctx.getBean("book", Book.class);
+		//book.setName("小雞吃米圖");
+		System.out.println(book);
+		
 	}
 }
