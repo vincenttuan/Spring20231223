66263b794a8b770a62754ed9b7d2aed21c78afff
MB-teacher <MB-teacher@MB-207-teacher>
2023-12-23 15:21:11 +0800

    java

--- SpringCore/src/spring/core/session2/bean/Book.java
+++ SpringCore/src/spring/core/session2/bean/Book.java
@@ -1,5 +1,17 @@
 package spring.core.session2.bean;
 
-public class Book {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+import lombok.Data;
+
+@Data
+@Component
+public class Book {
+	@Value("1")
+	private Integer id;
+	@Value("春樹秋霜圖")
+	private String name;
+	@Value("100")
+	private Integer price;
 }
