494bdcd256c390bad16851aaa340f252eb45525f
MB-teacher <MB-teacher@MB-207-teacher>
2023-12-23 14:06:15 +0800

    java

--- SpringCore/src/spring/core/session1/test/Test2.java
+++ SpringCore/src/spring/core/session1/test/Test2.java
@@ -10,14 +10,14 @@ public class Test2 {
 	public static void main(String[] args) {
 		// 指定 xml 設定檔
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-config.xml"); 
-		User user = ctx.getBean("user", User.class); // IOC
-		System.out.println(user);
-		User user2 = ctx.getBean("user2", User.class);
-		System.out.println(user2);
-		User user3 = ctx.getBean("user3", User.class);
-		System.out.println(user3);
-		User user4 = ctx.getBean("user4", User.class);
-		System.out.println(user4);
+//		User user = ctx.getBean("user", User.class); // IOC
+//		System.out.println(user);
+//		User user2 = ctx.getBean("user2", User.class);
+//		System.out.println(user2);
+//		User user3 = ctx.getBean("user3", User.class);
+//		System.out.println(user3);
+//		User user4 = ctx.getBean("user4", User.class);
+//		System.out.println(user4);
 	}
 
 }
