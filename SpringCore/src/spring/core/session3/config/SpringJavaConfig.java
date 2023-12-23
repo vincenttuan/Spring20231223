2f81f351ce09a86919858b26dc6b61cf147c0bc1
MB-teacher <MB-teacher@MB-207-teacher>
2023-12-23 15:51:51 +0800

    java

--- SpringCore/src/spring/core/session3/config/SpringJavaConfig.java
+++ SpringCore/src/spring/core/session3/config/SpringJavaConfig.java
@@ -1,8 +1,48 @@
 package spring.core.session3.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import spring.core.session3.bean.Employee;
+import spring.core.session3.bean.Title;
 
 @Configuration // Spring Java 配置檔
 public class SpringJavaConfig {
+	
+	@Bean
+	public Title title1() {
+		Title title = new Title();
+		title.setId(1);
+		title.setName("初階工程師");
+		title.setSalary(45000);
+		return title;
+	}
+	
+	@Bean
+	public Title title2() {
+		Title title = new Title();
+		title.setId(2);
+		title.setName("PM");
+		title.setSalary(100_000);
+		return title;
+	}
+	
+	@Bean
+	public Employee employee1() {
+		Employee employee = new Employee();
+		employee.setId(101);
+		employee.setName("John");
+		employee.setTitle(title1());
+		return employee;
+	}
+	
+	@Bean
+	public Employee employee2() {
+		Employee employee = new Employee();
+		employee.setId(202);
+		employee.setName("Mary");
+		employee.setTitle(title2());
+		return employee;
+	}
 	
 }
