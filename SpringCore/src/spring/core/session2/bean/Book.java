511ea04cdab3d79ca02eb07127944f745262ffc4
MB-teacher <MB-teacher@MB-207-teacher>
2023-12-23 15:22:14 +0800

    java

--- SpringCore/src/spring/core/session2/bean/Book.java
+++ SpringCore/src/spring/core/session2/bean/Book.java
@@ -5,10 +5,10 @@ import org.springframework.stereotype.Component;
 
 import lombok.Data;
 
-@Data
-@Component
+@Data // Lombok
+@Component // Spring
 public class Book {
-	@Value("1")
+	@Value("1") // Spring 
 	private Integer id;
 	@Value("春樹秋霜圖")
 	private String name;
