40987f0f7fae021d719250d21d336fea0703d485
MB-teacher <MB-teacher@MB-207-teacher>
2023-12-23 16:16:25 +0800

    java

--- SpringCore/src/spring/core/session3/bean/Job.java
+++ SpringCore/src/spring/core/session3/bean/Job.java
@@ -1,12 +1,14 @@
 package spring.core.session3.bean;
 
+import javax.inject.Named;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
 
 @Data
-@Component
+@Component(value="work") // @Component("work"), @Named("work")
 public class Job {
 	@Value("301")
 	private Integer id;
--- SpringCore/src/spring/core/session3/test/Test.java
+++ SpringCore/src/spring/core/session3/test/Test.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import spring.core.session3.bean.Employee;
+import spring.core.session3.bean.Job;
 import spring.core.session3.config.SpringJavaConfig;
 
 // 啟動參數: --add-opens java.base/java.lang=ALL-UNNAMED
@@ -15,5 +16,7 @@ public class Test {
 		Employee employee2 = ctx.getBean("employee2", Employee.class);
 		System.out.println(employee2);
 		
+		Job job = ctx.getBean("work", Job.class);
+		System.out.println(job);
 	}
 }
