39c0836f3c4131eaa96ef76978aee9d82d34d4d4
MB-teacher <MB-teacher@MB-207-teacher>
2023-12-23 14:13:50 +0800

    java

---
+++ SpringCore/src/spring/core/session1/test/Test3.java
@@ -0,0 +1,20 @@
+package spring.core.session1.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.core.session1.bean.Lotto;
+import spring.core.session1.bean.User;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		// 指定 xml 設定檔
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-config.xml"); 
+		Lotto lotto1 = ctx.getBean("lotto", Lotto.class);
+		Lotto lotto2 = ctx.getBean("lotto", Lotto.class);
+		System.out.println(lotto1);
+		System.out.println(lotto2);
+	}
+
+}
