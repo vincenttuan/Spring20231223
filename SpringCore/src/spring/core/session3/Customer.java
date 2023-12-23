724ddd2ee0098105c088a2402028d8a83e58cbc7
MB-teacher <MB-teacher@MB-207-teacher>
2023-12-23 15:37:21 +0800

    java

---
+++ SpringCore/src/spring/core/session3/Customer.java
@@ -0,0 +1,9 @@
+package spring.core.session3;
+
+import java.util.List;
+
+public class Customer {
+	private Integer id;
+	private String name;
+	private List<Product> products;
+}
---
+++ SpringCore/src/spring/core/session3/Product.java
@@ -0,0 +1,11 @@
+package spring.core.session3;
+
+import lombok.Data;
+
+@Data
+public class Product {
+	private Integer id;
+	private String name;
+	private Double price;
+	private Integer amount;
+}
