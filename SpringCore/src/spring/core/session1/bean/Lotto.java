6566b7bf4504daa1a753e3101c60c38fb9a24514
MB-teacher <MB-teacher@MB-207-teacher>
2023-12-23 14:12:28 +0800

    java

---
+++ SpringCore/src/spring/core/session1/bean/Lotto.java
@@ -0,0 +1,15 @@
+package spring.core.session1.bean;
+
+import java.util.Random;
+
+import lombok.Data;
+
+@Data
+public class Lotto {
+	private Integer number;
+	
+	public Lotto() {
+		System.out.println("執行 Lotto() 建構子");
+		number = new Random().nextInt(1000);
+	}
+}
