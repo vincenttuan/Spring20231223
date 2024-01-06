81a79b45709a02e070dc9556c49d5ee83ec319f6
MB-teacher <MB-teacher@MB-207-teacher>
2024-01-06 15:55:42 +0800

    java

--- SpringBootPSI/src/main/java/com/mvc/psi/entity/Department.java
+++ SpringBootPSI/src/main/java/com/mvc/psi/entity/Department.java
@@ -24,6 +24,10 @@ public class Department {
 	private String name;
 	
 	// mappedBy = "department" : 這個屬性是由 Employee 的 department 屬性來對應
+	// 意思是說, Employee 這個類別裡面有一個 department 屬性, 這個屬性是用來對應 Department 這個類別的, 這個屬性的名稱是 department, 所以要寫成 mappedBy = "department"
+	// 若不加上 mappedBy = "department", 則會產生一個中間表格, 用來對應 Department 與 Employee 的關係
+	// 中間表格的欄位名稱為 department_id, employee_id
+	// 中間表格的欄位名稱可以透過 @JoinColumn(name = "department_id") 來指定
 	@OneToMany(mappedBy = "department")
 	private Set<Employee> employees = new LinkedHashSet<>();
 }
