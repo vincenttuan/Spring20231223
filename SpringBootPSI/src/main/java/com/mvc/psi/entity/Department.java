e2e37805adc344b4a9def2b4e32f0cb5b0f1fcee
MB-teacher <MB-teacher@MB-207-teacher>
2024-01-06 15:51:04 +0800

    java

--- SpringBootPSI/src/main/java/com/mvc/psi/entity/Department.java
+++ SpringBootPSI/src/main/java/com/mvc/psi/entity/Department.java
@@ -1,5 +1,28 @@
 package com.mvc.psi.entity;
 
-public class Department {
+import java.util.LinkedHashSet;
+import java.util.Set;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "department")
+@Data
+public class Department {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(unique = true, nullable = false)
+	private String name;
+	
+	@OneToMany
+	private Set<Employee> employees = new LinkedHashSet<>();
 }
