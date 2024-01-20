eb6c7042bd1381bc593a240af0ec62e27d3f1746
MB-teacher <MB-teacher@MB-207-teacher>
2024-01-20 11:45:56 +0800

    java

--- SpringCore/src/spring/core/session4/tx/controller/BookController.java
+++ SpringCore/src/spring/core/session4/tx/controller/BookController.java
@@ -17,7 +17,7 @@ public class BookController {
 		System.out.println("buyOneBook OK !");
 	}
 	
-	// 買3本書(套書的概念)
+	// 買3本書
 	public void buyThreeBooks(String username, Integer bookId) {
 		bookOneService.buyOne(username, bookId);
 		bookOneService.buyOne(username, bookId);
