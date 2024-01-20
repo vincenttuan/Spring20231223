4ba51e4154c351f7821d582243b2a14fe677a501
MB-teacher <MB-teacher@MB-207-teacher>
2024-01-20 10:37:33 +0800

    java

---
+++ SpringCore/src/spring/core/session4/tx/dao/BookDao.java
@@ -0,0 +1,16 @@
+package spring.core.session4.tx.dao;
+
+public interface BookDao {
+	// 一般服務
+	Integer getBookPrice(Integer bookId); // 取得書本價格
+	Integer getBookStock(Integer bookId); // 取得書本庫存
+	Integer getWalletBalance(String username); // 取得客戶目前的餘額
+	
+	// 交易服務
+	Integer reduceBookStock(Integer bookId, Integer amountToReduce); // 更新書本庫存(減量)
+	Integer incrementBookStock(Integer bookId, Integer amountToIncrement);// 更新書本庫存(增量)
+	
+	Integer reduceWalletBalance(String username, Integer amountToReduce); // 更新錢包餘額(減量)
+	Integer incrementWalletBalance(String username, Integer amountToIncrement); // 更新錢包餘額(增量)
+	
+}
