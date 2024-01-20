02179ef7c926411c61a25b4035a64d116e5db37e
MB-teacher <MB-teacher@MB-207-teacher>
2024-01-20 14:49:51 +0800

    java

--- SpringCore/src/spring/core/session4/tx/service/BookThreeServiceImpl.java
+++ SpringCore/src/spring/core/session4/tx/service/BookThreeServiceImpl.java
@@ -2,6 +2,8 @@ package spring.core.session4.tx.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class BookThreeServiceImpl implements BookThreeService {
@@ -10,6 +12,7 @@ public class BookThreeServiceImpl implements BookThreeService {
 	private BookOneService bookOneService;
 	
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
 	public void buyThree(String username, Integer bookId) {
 		bookOneService.buyOne(username, bookId);
 		bookOneService.buyOne(username, bookId);
