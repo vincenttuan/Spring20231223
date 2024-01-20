bcd9f865d7c7aa33e16304f7ca5b5829df3fadfe
MB-teacher <MB-teacher@MB-207-teacher>
2024-01-20 11:56:17 +0800

    java

---
+++ SpringCore/src/spring/core/session4/tx/test/BookTest.java
@@ -0,0 +1,18 @@
+package spring.core.session4.tx.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.core.session4.tx.controller.BookController;
+
+public class BookTest {
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("jdbc-tx-config.xml");
+		
+		BookController bookController = ctx.getBean(BookController.class);
+		// John 買一本書
+		bookController.buyOneBook("John", 1);
+		
+		
+	}
+}
