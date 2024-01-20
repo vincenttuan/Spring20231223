7f59007e99db172414e6620e00d864e22eb0b231
MB-teacher <MB-teacher@MB-207-teacher>
2024-01-20 16:23:42 +0800

    java

--- SpringCore/src/spring/core/session4/tx/service/BookOneService.java
+++ SpringCore/src/spring/core/session4/tx/service/BookOneService.java
@@ -1,6 +1,9 @@
 package spring.core.session4.tx.service;
 
+import spring.core.session4.tx.exception.InsufficientAmount;
+import spring.core.session4.tx.exception.InsufficientStock;
+
 public interface BookOneService {
-	void buyOne(String username, Integer bookId); // 購書服務
+	void buyOne(String username, Integer bookId) throws InsufficientStock, InsufficientAmount; // 購書服務
 	void refundOne(String username, Integer bookId); // 退書服務
 }
