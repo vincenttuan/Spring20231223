c766a7d1f580955a2d5790c6a58c9c88ef7d43de
MB-teacher <MB-teacher@MB-207-teacher>
2024-01-20 11:50:27 +0800

    java

--- SpringCore/target/classes/spring/core/session4/tx/sql/tx.sql
+++ SpringCore/target/classes/spring/core/session4/tx/sql/tx.sql
@@ -27,7 +27,7 @@ create table if not exists book(
 +---------+-------------+
 */
 create table if not exists stock(
-	book_id int, -- 書號
+	book_id int primary key, -- 書號
     book_amount int, -- 存量
     foreign key(book_id) references book(book_id)
 );
