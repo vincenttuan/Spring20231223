1076cb6846872e039fdeddd1e13dc7a8418bd4aa
MB-teacher <MB-teacher@MB-207-teacher>
2024-01-20 11:12:12 +0800

    java

---
+++ SpringCore/src/spring/core/session4/tx/service/BookOneServiceImpl.java
@@ -0,0 +1,30 @@
+package spring.core.session4.tx.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import spring.core.session4.tx.dao.BookDao;
+
+@Repository
+public class BookOneServiceImpl implements BookOneService {
+	final Integer ONE = 1;
+	@Autowired
+	private BookDao bookDao;
+	
+	@Override
+	public void buyOne(String username, Integer bookId) {
+		// 1. 查詢書本價格
+		Integer bookPrice = bookDao.getBookPrice(bookId);
+		// 2. 減去庫存
+		bookDao.reduceBookStock(bookId, ONE);
+		// 3. 修改餘額
+		bookDao.reduceWalletBalance(username, bookPrice);
+	}
+
+	@Override
+	public void refundOne(String username, Integer bookId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
