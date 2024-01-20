06398010abf43d5f96b3892d5917420a6e7a574f
MB-teacher <MB-teacher@MB-207-teacher>
2024-01-20 11:19:00 +0800

    java

---
+++ SpringCore/src/spring/core/session4/tx/controller/BookController.java
@@ -0,0 +1,28 @@
+package spring.core.session4.tx.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import spring.core.session4.tx.service.BookOneService;
+
+@Controller
+public class BookController {
+	
+	@Autowired
+	private BookOneService bookOneService;
+	
+	// 買單本書
+	public void buyOneBook(String username, Integer bookId) {
+		bookOneService.buyOne(username, bookId);
+		System.out.println("buyOneBook OK !");
+	}
+	
+	// 買3本書(套書的概念)
+	public void buyThreeBooks(String username, Integer bookId) {
+		bookOneService.buyOne(username, bookId);
+		bookOneService.buyOne(username, bookId);
+		bookOneService.buyOne(username, bookId);
+		System.out.println("buyThreeBooks OK !");
+	}
+	
+}
