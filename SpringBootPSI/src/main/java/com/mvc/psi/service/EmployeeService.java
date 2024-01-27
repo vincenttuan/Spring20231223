02149d651798759812675944787a5b71a2932e7d
MB-teacher <MB-teacher@MB-207-teacher>
2024-01-27 12:16:57 +0800

    java

--- SpringBootPSI/src/main/java/com/mvc/psi/service/EmployeeService.java
+++ SpringBootPSI/src/main/java/com/mvc/psi/service/EmployeeService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mvc.psi.model.dto.EmployeeDto;
 import com.mvc.psi.model.po.Department;
@@ -25,6 +26,7 @@ public class EmployeeService {
 	private ModelMapper modelMapper;
 	
 	// 新增 I
+	@Transactional
 	public void add(EmployeeDto employeeDto) {
 		// DTO -> PO
 		Employee employee = modelMapper.map(employeeDto, Employee.class);
@@ -39,6 +41,7 @@ public class EmployeeService {
 	}
 	
 	// 新增 II
+	@Transactional
 	public void add(EmployeeDto employeeDto, Long departmentId) {
 		// DTO -> PO
 		Employee employee = modelMapper.map(employeeDto, Employee.class);
@@ -50,5 +53,23 @@ public class EmployeeService {
 		}
 	}
 	
+	// 修改
+	@Transactional
+	public void update(EmployeeDto employeeDto, Long id) {
+		Optional<Employee> employeeOpt = employeeRepository.findById(id);
+		if(employeeOpt.isPresent()) {
+			Employee employee = employeeOpt.get();
+			employee.setName(employeeDto.getName()); // 更新員工姓名
+			
+			Optional<Department> departmentOpt = departmentRepository.findById(employeeDto.getDepartment().getId());
+			if(departmentOpt.isPresent()) {
+				employee.setDepartment(departmentOpt.get()); // 更新部門
+				employeeRepository.save(employee);
+				return;
+			}
+			throw new RuntimeException("修改員工資料錯誤: 無此部門");
+		}
+		throw new RuntimeException("修改員工資料錯誤: 無此員工");
+	}
 	
 }
