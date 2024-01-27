2df2add8e73630ae8a710d4f1c729dc1f522468d
MB-teacher <MB-teacher@MB-207-teacher>
2024-01-27 16:29:17 +0800

    java

--- SpringBootPSI/src/main/java/com/mvc/psi/controller/DepartmentController.java
+++ SpringBootPSI/src/main/java/com/mvc/psi/controller/DepartmentController.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mvc.psi.model.dto.DepartmentDto;
@@ -14,11 +17,11 @@ import com.mvc.psi.service.DepartmentService;
 
 /*
  * URL 路徑設計
- * GET  /                       部門首頁
- * GET  /department/edit/{id}   單筆查詢(修改用)
- * POST /department/            新增
- * PUT  /department/{id}        修改
- * GET  /department/delete/{id} 刪除
+ * GET  /            部門首頁
+ * GET  /edit/{id}   單筆查詢(修改用)
+ * POST /            新增
+ * PUT  /{id}        修改
+ * GET  /delete/{id} 刪除
  * */
 @Controller
 @RequestMapping("/department")
@@ -35,5 +38,28 @@ public class DepartmentController {
 		return "department";
 	}
 	
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("departmentDto", departmentService.getDepartmentDtoById(id));
+		return "department-edit";
+	}
+	
+	@PostMapping("/")
+	public String add(DepartmentDto departmentDto) {
+		departmentService.add(departmentDto);
+		return "redirect:/department/"; // 重導到首頁
+	}
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable("id") Long id, DepartmentDto departmentDto) {
+		departmentService.update(departmentDto, id);
+		return "redirect:/department/"; // 重導到首頁
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable("id") Long id) {
+		departmentService.delete(id);
+		return "redirect:/department/"; // 重導到首頁
+	}
 	
 }
