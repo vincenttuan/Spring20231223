af59ddaadcea406715c51e0f4b6b73538b6c7c23
MB-teacher <MB-teacher@MB-207-teacher>
2024-01-27 16:08:59 +0800

    java

--- SpringBootPSI/src/main/java/com/mvc/psi/controller/DepartmentController.java
+++ SpringBootPSI/src/main/java/com/mvc/psi/controller/DepartmentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -26,6 +27,7 @@ public class DepartmentController {
 	@Autowired
 	private DepartmentService departmentService;
 	
+	@GetMapping("/")
 	public String index(@ModelAttribute DepartmentDto departmentDto, Model model) {
 		List<DepartmentDto> departmentDtos = departmentService.findAll();
 		model.addAttribute("departmentDtos", departmentDtos);
