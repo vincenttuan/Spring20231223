9da368b76d64fdddacd6ccb357e4f1645d0bf676
MB-teacher <MB-teacher@MB-207-teacher>
2024-01-27 11:00:58 +0800

    java

--- SpringBootPSI/src/main/java/com/mvc/psi/service/DepartmentService.java
+++ SpringBootPSI/src/main/java/com/mvc/psi/service/DepartmentService.java
@@ -1,5 +1,6 @@
 package com.mvc.psi.service;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,15 +14,23 @@ public class DepartmentService {
 	@Autowired
 	private DepartmentRepository departmentRepository;
 	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	// 新增
 	// Controller 會傳來一個 DepartmentDto 物件
 	// Service 要將 DTO(DepartmentDto) 轉成 PO(Department)
 	// 最後將 PO(Department) 傳給 Repository 新增
 	public void add(DepartmentDto departmentDto) {
 		// 將 DTO 轉 PO
+		/*
 		Department department = new Department();
 		department.setId(departmentDto.getId());
 		department.setName(departmentDto.getName());
+		*/
+		// 利用 modelMapper 轉換
+		// 注意: 會根據 DTO 與 PO 同名的屬性才會進行轉換
+		Department department = modelMapper.map(departmentDto, Department.class);
 		// 儲存
 		departmentRepository.save(department);
 	}
