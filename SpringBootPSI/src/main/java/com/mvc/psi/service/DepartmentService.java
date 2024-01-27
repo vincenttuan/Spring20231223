6defd8f1144e1d3142cf35de80ad2c7e98ae8271
MB-teacher <MB-teacher@MB-207-teacher>
2024-01-27 11:05:22 +0800

    java

--- SpringBootPSI/src/main/java/com/mvc/psi/service/DepartmentService.java
+++ SpringBootPSI/src/main/java/com/mvc/psi/service/DepartmentService.java
@@ -1,8 +1,11 @@
 package com.mvc.psi.service;
 
+import java.util.Optional;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mvc.psi.model.dto.DepartmentDto;
 import com.mvc.psi.model.po.Department;
@@ -21,6 +24,7 @@ public class DepartmentService {
 	// Controller 會傳來一個 DepartmentDto 物件
 	// Service 要將 DTO(DepartmentDto) 轉成 PO(Department)
 	// 最後將 PO(Department) 傳給 Repository 新增
+	@Transactional
 	public void add(DepartmentDto departmentDto) {
 		// 將 DTO 轉 PO
 		/*
@@ -36,6 +40,19 @@ public class DepartmentService {
 	}
 	
 	// 修改
+	@Transactional
+	public void update(DepartmentDto departmentDto, Long id) {
+		// 根據 id 找到可以修改的紀錄
+		Optional<Department> departmentOpt = departmentRepository.findById(id);
+		if(departmentOpt.isPresent()) {
+			Department department = departmentOpt.get();
+			// 針對要修改的欄位進行修改
+			department.setName(departmentDto.getName());
+			// 修改
+			departmentRepository.save(department);
+		}
+	}
+	
 	
 	// 刪除
 	
