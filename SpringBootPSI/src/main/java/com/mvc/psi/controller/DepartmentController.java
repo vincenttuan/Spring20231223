8a3b8696530e8f330c85ada8f62e9508dc435597
MB-teacher <MB-teacher@MB-207-teacher>
2024-01-27 14:56:56 +0800

    java

---
+++ SpringBootPSI/src/main/java/com/mvc/psi/controller/DepartmentController.java
@@ -0,0 +1,17 @@
+package com.mvc.psi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+ * URL 路徑設計
+ * GET  /department/edit/{id} 單筆查詢(修改用)
+ * POST /department/ 新增
+ * PUT  /department/{id} 修改
+ * GET  /department/delete/{id} 刪除
+ * */
+@Controller
+@RequestMapping("/department")
+public class DepartmentController {
+
+}
---
+++ SpringBootPSI/src/main/java/com/mvc/psi/controller/EmployeeController.java
@@ -0,0 +1,17 @@
+package com.mvc.psi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+ * URL 路徑設計
+ * GET  /employee/edit/{id} 單筆查詢(修改用)
+ * POST /employee/ 新增
+ * PUT  /employee/{id} 修改
+ * GET  /employee/delete/{id} 刪除
+ * */
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+}
