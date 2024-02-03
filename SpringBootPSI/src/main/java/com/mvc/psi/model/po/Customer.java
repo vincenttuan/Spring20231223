c2a70e4e72269465d9ee4603f53b724673e28208
MB-teacher <MB-teacher@MB-207-teacher>
2024-02-03 10:31:20 +0800

    java

---
+++ SpringBootPSI/src/main/java/com/mvc/psi/model/po/Customer.java
@@ -0,0 +1,29 @@
+package com.mvc.psi.model.po;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customer")
+@Getter
+@Setter
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // 客戶序號
+	
+	@Column
+	private String name; // 客戶姓名
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + "]";
+	}
+	
+}
