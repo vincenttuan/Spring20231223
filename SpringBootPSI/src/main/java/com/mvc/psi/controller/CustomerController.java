8f537041b1c59a52a1f128be59e5ec2e1ffb234f
MB-teacher <MB-teacher@MB-207-teacher>
2024-02-03 15:04:35 +0800

    java

--- SpringBootPSI/src/main/java/com/mvc/psi/controller/CustomerController.java
+++ SpringBootPSI/src/main/java/com/mvc/psi/controller/CustomerController.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mvc.psi.model.dto.CustomerDto;
@@ -34,5 +37,29 @@ public class CustomerController {
 		return "customer";
 	}
 	
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable("id") Long id, Model model) {
+		CustomerDto customerDto = customerService.getCustomerDtoById(id);
+		model.addAttribute("customerDto", customerDto);
+		return "customer-edit";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable("id") Long id) {
+		customerService.delete(id);
+		return "redirect:/customer/";
+	}
+	
+	@PostMapping("/")
+	public String add(CustomerDto customerDto) {
+		customerService.add(customerDto);
+		return "redirect:/customer/";
+	}
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable("id") Long id, CustomerDto customerDto) {
+		customerService.update(customerDto, id);
+		return "redirect:/customer/";
+	}
 	
 }
