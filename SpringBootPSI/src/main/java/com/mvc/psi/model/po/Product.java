4a991da585269335aae764881fe19e857a22e88d
MB-teacher <MB-teacher@MB-207-teacher>
2024-02-03 11:56:35 +0800

    java

---
+++ SpringBootPSI/src/main/java/com/mvc/psi/model/po/Product.java
@@ -0,0 +1,33 @@
+package com.mvc.psi.model.po;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // 商品代號
+	
+	@Column
+	private String name; // 商品名稱
+	
+	@Column
+	private Integer cost; // 商品成本
+	
+	@Column
+	private Integer price; // 商品定價
+	
+}
