1a59007598e040437db0ad2f0f9beb2306bcc155
MB-teacher <MB-teacher@MB-207-teacher>
2024-02-03 11:33:03 +0800

    java

--- SpringBootPSI/src/main/java/com/mvc/psi/service/CustomerService.java
+++ SpringBootPSI/src/main/java/com/mvc/psi/service/CustomerService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mvc.psi.model.dto.CustomerDto;
 import com.mvc.psi.model.po.Customer;
@@ -21,6 +22,7 @@ public class CustomerService {
 	private ModelMapper modelMapper;
 	
 	// 新增
+	@Transactional
 	public void add(CustomerDto customerDto) {
 		// Dto 轉 Po
 		Customer customer = modelMapper.map(customerDto, Customer.class);
@@ -28,8 +30,16 @@ public class CustomerService {
 	}
 	
 	// 修改
+	@Transactional
 	public void update(CustomerDto customerDto, Long id) {
 		Optional<Customer> customerOpt = customerRepository.findById(id);
+		
+		customerOpt.ifPresentOrElse(customer -> {
+			customer.setName(customerDto.getName());
+			customerRepository.save(customer);
+		}, () -> {throw new RuntimeException("修改失敗: 無此資料");});
+		
+		/*
 		if(customerOpt.isPresent()) {
 			Customer customer = customerOpt.get();
 			customer.setName(customerDto.getName());
@@ -37,9 +47,11 @@ public class CustomerService {
 			return;
 		}
 		throw new RuntimeException("修改失敗: 無此資料");
+		*/
 	}
 	
 	// 刪除
+	@Transactional
 	public void delete(Long id) {
 		Optional<Customer> customerOpt = customerRepository.findById(id);
 		if(customerOpt.isPresent()) {
