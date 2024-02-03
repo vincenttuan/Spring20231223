03432515185c76dbfef96aa88de697bcd375a813
MB-teacher <MB-teacher@MB-207-teacher>
2024-02-03 13:38:06 +0800

    java

--- SpringBootPSI/src/main/java/com/mvc/psi/SpringBootPsiApplication.java
+++ SpringBootPSI/src/main/java/com/mvc/psi/SpringBootPsiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
+//@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 //@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class SpringBootPsiApplication {
 
