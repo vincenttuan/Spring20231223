bd6a0cc2071b74a262e01be91d2d0c8bf623c988
MB-teacher <MB-teacher@MB-207-teacher>
2024-02-24 11:12:07 +0800

    java

---
+++ SpringBootPSI/src/main/java/com/mvc/psi/model/dto/PurchaseDto.java
@@ -0,0 +1,41 @@
+package com.mvc.psi.model.dto;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PurchaseDto {
+	private Long id; // 採購單序號
+	
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date date; // 採購日期
+	
+	private SupplierDto supplier;
+	
+	private EmployeeDto employee;
+	
+	private List<PurchaseItemDto> purchaseItems = new ArrayList<>();
+	
+	// 計算採購單總價
+	public Integer getTotal() {
+		if(purchaseItems.size() == 0) {
+			return 0;
+		}
+		return purchaseItems.stream()
+				.mapToInt(item -> item.getAmount() * item.getProduct().getCost())
+				.sum();
+	}
+}
+
---
+++ SpringBootPSI/src/main/java/com/mvc/psi/model/dto/PurchaseItemDto.java
@@ -0,0 +1,17 @@
+package com.mvc.psi.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PurchaseItemDto {
+	
+	private Long id; // 採購項目序號
+	private Integer amount; // 採購數量
+	private PurchaseDto purchase;
+	private ProductDto product;
+	
+}
+
+
