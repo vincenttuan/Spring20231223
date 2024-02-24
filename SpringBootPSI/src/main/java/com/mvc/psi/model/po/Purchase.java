ef3139c9cd7c1adc40e3d469fbb4c75537a22496
MB-teacher <MB-teacher@MB-207-teacher>
2024-02-24 11:06:11 +0800

    java

--- SpringBootPSI/src/main/java/com/mvc/psi/model/po/Purchase.java
+++ SpringBootPSI/src/main/java/com/mvc/psi/model/po/Purchase.java
@@ -1,16 +1,21 @@
 package com.mvc.psi.model.po;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Set;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -39,7 +44,12 @@ public class Purchase {
 	@JoinColumn(name = "employee_id") // 員工(外鍵)
 	@ManyToOne
 	private Employee employee; // 員工
-
+	
+	// FetchType.EAGER 主動查詢
+	// FetchType.LAZY 被動查詢
+	@OneToMany(mappedBy = "purchase", fetch = FetchType.EAGER)
+	private List<PurchaseItem> purchaseItems = new ArrayList<PurchaseItem>(); // 採購單明細集合
+	
 	@Override
 	public String toString() {
 		return "Purchase [id=" + id + ", date=" + date + ", supplier=" + supplier + ", employee=" + employee + "]";
