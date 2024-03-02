6425e1fd30a4498b48d82f1edbcac067f7edea29
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-02 12:18:01 +0800

    java

--- SpringCore/src/spring/core/session5/sta/CalcTest.java
+++ SpringCore/src/spring/core/session5/sta/CalcTest.java
@@ -3,6 +3,7 @@ package spring.core.session5.sta;
 public class CalcTest {
 
 	public static void main(String[] args) {
+		// 在 Add.java 與 Div.java 不可以變動的情況下
 		// 如何利用 CalcProxy 可以處理除法中分母 = 0 的問題
 		// ex: 例外通知: 分母不可 = 0
 		// 如何利用 CalcProxy 可以處理 x 或 y = null 的問題
