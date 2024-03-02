268e64d2b0e96da15e42bfe4179476cfb632644c
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-02 16:04:40 +0800

    java

---
+++ SpringCore/src/spring/core/session6/aop/Calc.java
@@ -0,0 +1,9 @@
+package spring.core.session6.aop;
+
+public interface Calc {
+	Integer add(Integer x, Integer y);
+	Integer sub(Integer x, Integer y);
+	Integer mul(Integer x, Integer y);
+	Integer div(Integer x, Integer y);
+	
+}
