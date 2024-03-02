9aafeee5d4b0a3aa3af508c546622164fab3e34f
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-02 15:07:27 +0800

    java

--- SpringCore/src/spring/core/session5/dyn/MyLoggerAspect.java
+++ SpringCore/src/spring/core/session5/dyn/MyLoggerAspect.java
@@ -1,6 +1,12 @@
 package spring.core.session5.dyn;
 
 import java.lang.reflect.Method;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 
 // 紀錄所有使用者下達參數的切面程式
 public class MyLoggerAspect {
@@ -9,6 +15,16 @@ public class MyLoggerAspect {
 	public static void before(Method method, Object[] args) {
 		if(args != null) {
 			String filePath = "src/spring/core/session5/dyn/log.txt";
+			String content = method.getName() + " " + Arrays.toString(args);
+			try {
+				Files.write(
+						Paths.get(filePath), // 存檔路徑
+						content.getBytes(StandardCharsets.UTF_8), // 轉 byte[] 陣列並編碼
+						StandardOpenOption.CREATE, // 若文件不存在, 則會創建新的文件
+						StandardOpenOption.APPEND); // 新的資料添加到文件末尾
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 			
 		}
 	}
