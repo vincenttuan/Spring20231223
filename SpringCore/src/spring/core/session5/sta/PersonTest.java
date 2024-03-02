f7afa96991071c5d2117121738dfd6566f32e65e
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-02 11:43:22 +0800

    java

---
+++ SpringCore/src/spring/core/session5/sta/PersonTest.java
@@ -0,0 +1,14 @@
+package spring.core.session5.sta;
+
+public class PersonTest {
+
+	public static void main(String[] args) {
+		// 不使用代理
+		Person man = new Man();
+		Person woman = new Woman();
+		man.work();
+		woman.work();
+
+	}
+
+}
