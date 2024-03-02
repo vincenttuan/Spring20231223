5b4af56e95df4dd74ff3b974119d71b998dad967
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-02 12:20:03 +0800

    java

--- SpringCore/src/spring/core/session5/sta/CalcTest.java
+++ SpringCore/src/spring/core/session5/sta/CalcTest.java
@@ -8,6 +8,12 @@ public class CalcTest {
 		// ex: 例外通知: 分母不可 = 0
 		// 如何利用 CalcProxy 可以處理 x 或 y = null 的問題
 		// ex: 例外通知: x 不可是 null, y 不可是 null
+		// 測試:
+		// add.calculate(20, 10) --> 得到 30
+		// div.calculate(20, 10) --> 得到 2
+		// add.calculate(null, 10) --> 例外通知: x 不可是 null
+		// add.calculate(null, null) --> 例外通知: x 不可是 null, 例外通知: y 不可是 null
+		// div.calculate(20, 0) --> 例外通知: 分母不可 = 0
 		
 	}
 
