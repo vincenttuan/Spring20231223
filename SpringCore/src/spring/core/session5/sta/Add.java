93a215a01ba7332201c1a7ff2962a00eb8df60a3
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-02 12:14:13 +0800

    java

---
+++ SpringCore/src/spring/core/session5/sta/Add.java
@@ -0,0 +1,10 @@
+package spring.core.session5.sta;
+
+public class Add implements Calc {
+
+	@Override
+	public int calculate(int x, int y) {
+		return x + y;
+	}
+
+}
---
+++ SpringCore/src/spring/core/session5/sta/Div.java
@@ -0,0 +1,10 @@
+package spring.core.session5.sta;
+
+public class Div implements Calc {
+
+	@Override
+	public int calculate(int x, int y) {
+		return x / y;
+	}
+
+}
