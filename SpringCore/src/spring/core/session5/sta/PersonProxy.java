276631321b13e1d63d3ada22f06c1e78ff500319
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-02 12:08:12 +0800

    java

--- SpringCore/src/spring/core/session5/sta/PersonProxy.java
+++ SpringCore/src/spring/core/session5/sta/PersonProxy.java
@@ -13,14 +13,18 @@ public class PersonProxy implements Person {
 	
 	@Override
 	public void work() {
-		// "公用"邏輯
+		// "公用"邏輯-前置通知
 		System.out.println("出門戴口罩");
-		
-		// "業務"邏輯
-		person.work();
-		
-		// "公用"邏輯
-		System.out.println("回家脫口罩");
+		try {
+			// "業務"邏輯
+			person.work();
+		} catch (Exception e) {
+			// "公用"邏輯-例外通知
+			System.out.println("有例外發生: " + e);
+		} finally {
+			// "公用"邏輯-後置通知(保證一定會執行)
+			System.out.println("回家脫口罩");
+		} 
 	}
 	
 }
