1eec36cc4c73765ba1e1ea39b04dcf2039ab5e32
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-02 12:03:30 +0800

    java

--- SpringCore/src/spring/core/session5/sta/PersonTest.java
+++ SpringCore/src/spring/core/session5/sta/PersonTest.java
@@ -4,11 +4,19 @@ public class PersonTest {
 
 	public static void main(String[] args) {
 		// 不使用代理
+		/*
 		Person man = new Man();
 		Person woman = new Woman();
 		man.work();
 		woman.work();
-
+		*/
+		// 使用代理
+		Person man = new PersonProxy(new Man());
+		Person woman = new PersonProxy(new Woman());
+		man.work();
+		woman.work();
+		
+		
 	}
 
 }
--- SpringCore/src/spring/core/session5/sta/Woman.java
+++ SpringCore/src/spring/core/session5/sta/Woman.java
@@ -5,13 +5,13 @@ public class Woman implements Person {
 	@Override
 	public void work() {
 		// 公用邏輯
-		System.out.println("出門戴口罩");
+		//System.out.println("出門戴口罩");
 				
 		// 業務邏輯
 		System.out.println("Woman 市場買菜");
 		
 		// 公用邏輯
-		System.out.println("回家脫口罩");
+		//System.out.println("回家脫口罩");
 	}
 	
 }
