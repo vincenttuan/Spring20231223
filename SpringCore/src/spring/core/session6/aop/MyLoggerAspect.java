317dcc291ecae2b74b38ed70c828d12f249265e3
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-02 16:31:59 +0800

    java

--- SpringCore/src/spring/core/session6/aop/MyLoggerAspect.java
+++ SpringCore/src/spring/core/session6/aop/MyLoggerAspect.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -15,7 +16,31 @@ import org.springframework.stereotype.Component;
 public class MyLoggerAspect {
 	private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	
+	// 建立一個切入點的邏輯方法
+	@Pointcut(value = "execution(public Integer spring.core.session6.aop.CalcImpl.add(Integer, Integer))")
+	public void ptAdd() {}
+	
+	@Pointcut(value = "execution(public Integer spring.core.session6.aop.CalcImpl.sub(Integer, Integer))")
+	public void ptSub() {}
+	
+	@Pointcut(value = "execution(public Integer spring.core.session6.aop.CalcImpl.mul(Integer, Integer))")
+	public void ptMul() {}
+	
+	@Pointcut(value = "execution(public Integer spring.core.session6.aop.CalcImpl.div(Integer, Integer))")
+	public void ptDiv() {}
+	
+	@Pointcut(value = "execution(public Integer spring.core.session6.aop.CalcImpl.*(..))")
+	public void ptAll() {}
+	
 	@Before(value = "execution(public Integer spring.core.session6.aop.CalcImpl.add(Integer, Integer))")
+	//@Before(value = "execution(public Integer spring.core.session6.aop.CalcImpl.*(Integer, Integer))")
+	//@Before(value = "execution(public Integer spring.core.session6.aop.CalcImpl.*(..))")
+	//@Before(value = "execution(* spring.core.session6.aop.CalcImpl.*(..))")
+	//@Before(value = "execution(* spring.core.session6.aop.*.*(..))")
+	//@Before(value = "execution(* *(..))")
+	//@Before(value = "ptAdd()")
+	//@Before(value = "ptAdd() && ptMul()")
+	//@Before(value = "ptAll() && !ptDiv()") // 切入點表達式支援邏輯運算子: &&, ||, !
 	public void beforeAdvice(JoinPoint joinPoint) {
 		String methodName = joinPoint.getSignature().getName(); // 方法名稱
 		Object[] args = joinPoint.getArgs(); // 方法參數
