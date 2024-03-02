45cd7e50d62533d1aad7aecca630c906d619e662
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-02 11:41:58 +0800

    java

---
+++ SpringCore/src/spring/core/session5/sta/Man.java
@@ -0,0 +1,13 @@
+package spring.core.session5.sta;
+
+public class Man implements Person {
+
+	@Override
+	public void work() {
+		
+		// 業務邏輯
+		System.out.println("Man 上班工作");
+		
+	}
+	
+}
---
+++ SpringCore/src/spring/core/session5/sta/Woman.java
@@ -0,0 +1,13 @@
+package spring.core.session5.sta;
+
+public class Woman implements Person {
+
+	@Override
+	public void work() {
+		
+		// 業務邏輯
+		System.out.println("Woman 市場買菜");
+		
+	}
+	
+}
