e1de688a4c09b598cd692da0550c55fe4e41b66d
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-02 16:37:12 +0800

    java

---
+++ SpringCore/src/spring/core/session6/test/AOPTest.java
@@ -0,0 +1,13 @@
+package spring.core.session6.test;
+
+//執行時要加入: JVM 啟動參數
+//--add-opens java.base/java.lang=ALL-UNNAMED
+//允許 Java 9 以上版本使用反射(java.lang.reflect)內部 API
+public class AOPTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
