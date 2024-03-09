8c8781ae25a240790b312938411fefbfc05eb5ae
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-09 14:54:22 +0800

    java

--- SpringBootWebSocket/src/main/java/com/mvc/service/QuoteService.java
+++ SpringBootWebSocket/src/main/java/com/mvc/service/QuoteService.java
@@ -1,5 +1,8 @@
 package com.mvc.service;
 
+import java.net.URL;
+import java.util.Scanner;
+
 import org.springframework.stereotype.Service;
 
 // 取得股價報價服務
@@ -7,14 +10,16 @@ import org.springframework.stereotype.Service;
 public class QuoteService {
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		/*
 		 * 股票交易資訊
 		 * https://query1.finance.yahoo.com/v7/finance/download/2330.TW
 		 * Date     Open High Low Close Adj Close Volume
 		 * 2024/3/8 795  796  772 784   784       87139744
 		 * */
-		
+		String url = String.format("https://query1.finance.yahoo.com/v7/finance/download/%s", "2330.TW");
+		String csvData = new Scanner(new URL(url).openStream()).useDelimiter("\\A").next();
+		System.out.println(csvData);
 		
 	}
 	
