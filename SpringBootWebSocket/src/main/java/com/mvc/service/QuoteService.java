92ebdd7ba3502376d8e931f1b879dd100c4c1373
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-09 14:49:14 +0800

    java

---
+++ SpringBootWebSocket/src/main/java/com/mvc/service/QuoteService.java
@@ -0,0 +1,21 @@
+package com.mvc.service;
+
+import org.springframework.stereotype.Service;
+
+// 取得股價報價服務
+@Service
+public class QuoteService {
+	
+	
+	public static void main(String[] args) {
+		/*
+		 * 股票交易資訊
+		 * https://query1.finance.yahoo.com/v7/finance/download/2330.TW
+		 * Date     Open High Low Close Adj Close Volume
+		 * 2024/3/8 795  796  772 784   784       87139744
+		 * */
+		
+		
+	}
+	
+}
