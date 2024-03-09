790752890314889cf5ad9dbd95e2031aaa31253f
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-09 11:45:46 +0800

    java

--- SpringBootWebSocket/src/main/java/com/mvc/model/Greeting.java
+++ SpringBootWebSocket/src/main/java/com/mvc/model/Greeting.java
@@ -1,3 +1,4 @@
 package com.mvc.model;
 
+// 自訂 WebSocket 回傳的資料格式
 public record Greeting(Boolean status, String content) {}
--- SpringBootWebSocket/src/main/java/com/mvc/model/Message.java
+++ SpringBootWebSocket/src/main/java/com/mvc/model/Message.java
@@ -3,4 +3,5 @@ package com.mvc.model;
 // jdk 16 開始: record 是指定一個不可變的資料物件(屬性資料都使用 final 修飾)
 // jdk 21 release
 // 將當於傳統 Java 物件的 getter、equals、hashCode、toString 等方法都自動產生
+// 自訂 WebSocket 接收的資料格式
 public record Message(String content) {}
