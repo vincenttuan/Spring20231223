6a3c1ebb7d713409951beb29bd90ed323df822b8
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-09 12:09:51 +0800

    java

--- SpringBootWebSocket/src/main/resources/static/js/app.js
+++ SpringBootWebSocket/src/main/resources/static/js/app.js
@@ -1,6 +1,33 @@
 const url = 'ws://localhost:8080/spring-boot-websocket';
+
 // 訂閱
 const topic1 = "/topic/greetings";
 
 // 調用服務
 const app_register = "/app/register";
+
+const clinet = new StompJs.Client({
+	brokerURL: url
+});
+
+var buttonConnect;
+var buttonDisconnect;
+
+document.addEventListener("DOMContentLoaded", function() {
+	buttonConnect = document.getElementById("connect");
+	buttonDisconnect = document.getElementById("disconnect");
+	
+	buttonConnect.addEventListener("click", (e) => {
+		connect();
+		e.preventDefault(); // 停止該元件預設行為
+	});
+	
+	buttonDisconnect.addEventListener("click", (e) => {
+		disconnect();
+		e.preventDefault(); // 停止該元件預設行為
+	});
+	
+});
+
+
+
