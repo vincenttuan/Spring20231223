94767c2b01b25e0d930c5c6dbbd527b0c117734a
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-09 11:49:58 +0800

    java

--- SpringBootWebSocket/src/main/java/com/mvc/controller/request/RegisterController.java
+++ SpringBootWebSocket/src/main/java/com/mvc/controller/request/RegisterController.java
@@ -3,6 +3,7 @@ package com.mvc.controller.request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import com.mvc.model.Greeting;
 import com.mvc.model.Message;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -20,7 +21,12 @@ public class RegisterController {
 	// Greeting 回傳的資料結構
 	public void register(Message message) throws Exception {
 		String username = message.content();
-		
+		Boolean status = username.equals("john");
+		String content = status ? "註冊成功" : "註冊失敗";
+		Greeting greeting = new Greeting(status, content);
+		// 因為 server 會透過 "/topic/greetings" 通道傳送訊息
+		// 所以 client 端也必須訂閱 "/topic/greetings" 通道接收訊息
+		simpMessagingTemplate.convertAndSend("/topic/greetings", greeting);
 	}
 	
 }
