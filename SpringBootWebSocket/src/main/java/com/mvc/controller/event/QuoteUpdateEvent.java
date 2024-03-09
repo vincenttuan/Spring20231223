63398ed8debc005996be23ccecf8131f67f1bacf
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-09 16:08:31 +0800

    java

--- SpringBootWebSocket/src/main/java/com/mvc/controller/event/QuoteUpdateEvent.java
+++ SpringBootWebSocket/src/main/java/com/mvc/controller/event/QuoteUpdateEvent.java
@@ -1,5 +1,23 @@
 package com.mvc.controller.event;
 
-public class QuoteUpdateEvent {
+import java.time.Clock;
+
+import org.springframework.context.ApplicationEvent;
+
+import com.mvc.model.Quote;
+
+public class QuoteUpdateEvent extends ApplicationEvent {
+	
+	private Quote quote;
+	
+	public QuoteUpdateEvent(Object source, Quote quote) {
+		super(source);
+		this.quote = quote;
+	}
+	
+	public Quote getQuote() {
+		return quote;
+	}
+	
 
 }
