6f82cccff6432df6b98ac11bce5d9a3c7a80be1e
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-16 14:07:29 +0800

    java

---
+++ SpringBootWebSocket/src/main/java/com/mvc/model/CommissionReturn.java
@@ -0,0 +1,7 @@
+package com.mvc.model;
+
+// 委託單
+// 委託編號, 委託狀態, 委託時間, 買賣, Order(下單資料)
+public class CommissionReturn {
+
+}
--- SpringBootWebSocket/src/main/java/com/mvc/model/Order.java
+++ SpringBootWebSocket/src/main/java/com/mvc/model/Order.java
@@ -1,4 +1,5 @@
 package com.mvc.model;
 
-// 下單資訊
+// Order(下單資料)
+// 帳號, 買賣方向, 股票代號, 價格, 數量
 public record Order(String username, String bs, String symbol, Double price, Integer amount) {}
