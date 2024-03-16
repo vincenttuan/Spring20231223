0d9295183ca8b0db280dea4288a63aa7b15cd7fa
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-16 11:17:06 +0800

    java

--- SpringBootWebSocket/src/main/java/com/mvc/config/WebSecurityConfig.java
+++ SpringBootWebSocket/src/main/java/com/mvc/config/WebSecurityConfig.java
@@ -24,14 +24,12 @@ public class WebSecurityConfig {
         http
             .csrf(csrf -> csrf.disable()) // 禁用CSRF（跨站請求偽造）保護
             .authorizeHttpRequests((authz) -> authz
-                .requestMatchers("/image", "/logout.html", "/css/**", "/js/**", "/images/**", "/favicon.ico").permitAll() // 允許對/login和靜態資源（如CSS、JS、圖片）的訪問，無需認證
+                .requestMatchers("/image/**", "/logout.html", "/css/**", "/js/**", "/images/**", "/favicon.ico").permitAll() // 允許對/login和靜態資源（如CSS、JS、圖片）的訪問，無需認證
                 .requestMatchers("/manager").hasRole("ADMIN") // 只有ADMIN角色可以访问/manager
                 .requestMatchers("/", "/index.html", "/hello").hasAnyRole("USER", "ADMIN") // USER和ADMIN角色可以访问/和/hello
                 .anyRequest().authenticated()) // 要求所有其他請求都必須經過認證
-            .formLogin(form -> form
-               .permitAll()) // 允許所有用戶訪問表單登錄頁面
-            .logout(logout -> logout
-            	.logoutSuccessUrl("/logout.html") // 登出成功後跳轉到 /login
+            .formLogin(form -> form.permitAll()) // 允許所有用戶訪問表單登錄頁面
+            .logout(logout -> logout.logoutSuccessUrl("/logout.html") // 登出成功後跳轉到 /login
              )
             .cors(Customizer.withDefaults()); // 啟用CORS（跨域資源共享），使用預設配置
 
@@ -50,6 +48,12 @@ public class WebSecurityConfig {
             .password(encoder.encode("1234"))
             .roles("USER") // 將自動添加前娺字 "ROLE_"
             .build();
+        
+        UserDetails user2 = User.builder()
+                .username("mary")
+                .password(encoder.encode("1234"))
+                .roles("USER") // 將自動添加前娺字 "ROLE_"
+                .build();
 
         UserDetails admin = User.builder()
             .username("admin")
@@ -57,7 +61,7 @@ public class WebSecurityConfig {
             .roles("ADMIN") // 將自動添加前娺字 "ROLE_"
             .build();
 
-        return new InMemoryUserDetailsManager(user, admin);
+        return new InMemoryUserDetailsManager(user, user2, admin);
     }
     
 }
