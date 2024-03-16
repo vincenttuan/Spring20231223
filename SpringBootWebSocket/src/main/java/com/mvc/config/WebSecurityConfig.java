df61b40367b9788f71774c86da233a977a4ed9ca
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-16 11:24:19 +0800

    java

--- SpringBootWebSocket/src/main/java/com/mvc/config/WebSecurityConfig.java
+++ SpringBootWebSocket/src/main/java/com/mvc/config/WebSecurityConfig.java
@@ -25,8 +25,8 @@ public class WebSecurityConfig {
             .csrf(csrf -> csrf.disable()) // 禁用CSRF（跨站請求偽造）保護
             .authorizeHttpRequests((authz) -> authz
                 .requestMatchers("/image/**", "/logout.html", "/css/**", "/js/**", "/images/**", "/favicon.ico").permitAll() // 允許對/login和靜態資源（如CSS、JS、圖片）的訪問，無需認證
-                .requestMatchers("/manager").hasRole("ADMIN") // 只有ADMIN角色可以访问/manager
-                .requestMatchers("/", "/index.html", "/hello").hasAnyRole("USER", "ADMIN") // USER和ADMIN角色可以访问/和/hello
+                //.requestMatchers("/manager").hasRole("ADMIN") // 只有ADMIN角色可以访问/manager
+                //.requestMatchers("/", "/index.html", "/hello").hasAnyRole("USER", "ADMIN") // USER和ADMIN角色可以访问/和/hello
                 .anyRequest().authenticated()) // 要求所有其他請求都必須經過認證
             .formLogin(form -> form.permitAll()) // 允許所有用戶訪問表單登錄頁面
             .logout(logout -> logout.logoutSuccessUrl("/logout.html")) // 登出成功後跳轉到 /login
