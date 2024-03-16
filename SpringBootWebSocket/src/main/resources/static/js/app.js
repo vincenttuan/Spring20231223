e1245858d2831d7faea477b863298bf211c7b477
MB-teacher <MB-teacher@MB-207-teacher>
2024-03-16 13:54:16 +0800

    java

--- SpringBootWebSocket/src/main/resources/static/js/app.js
+++ SpringBootWebSocket/src/main/resources/static/js/app.js
@@ -90,11 +90,15 @@ function updateQuoteList(quote) {
 	if(!quoteRow) { // 建立新 row
 		quoteRow = document.createElement('tr');
 		quoteRow.setAttribute('id', 'row-' + quote.symbol);
-		quoteRow.innerHTML = `<td>${quote.symbol}</td><td>${quote.成交}</td><td>${quote.總量}</td><td style='cursor: pointer' title='按我一下可以移除' onclick='cancelOrderSymbol("${quote.symbol}")'>X</td><td></td><td></td>`;
 		quotesBody.appendChild(quoteRow);
-	} else { // 更新 row
-		quoteRow.innerHTML = `<td>${quote.symbol}</td><td>${quote.成交}</td><td>${quote.總量}</td><td style='cursor: pointer' title='按我一下可以移除' onclick='cancelOrderSymbol("${quote.symbol}")'>X</td><td></td><td></td>`;
 	}
+	quoteRow.innerHTML = `<td>${quote.symbol}</td>
+					<td>${quote.成交}</td>
+					<td>${quote.總量}</td>
+					<td style='cursor: pointer' title='按我一下可以移除' onclick='cancelOrderSymbol("${quote.symbol}")'>X</td>
+					<td style='cursor: pointer' title='按我一下可以買進' onclick='buyOrder("${quote.symbol}", ${quote.price})'>X</td>
+					<td style='cursor: pointer' title='按我一下可以賣出' onclick='sellOrder("${quote.symbol}", ${quote.price})'>X</td>`;
+
 }
 
 function cancelOrderSymbol(symbol) {
